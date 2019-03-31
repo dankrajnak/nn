6f562ed063578d9a39630330f87c68778771aa9e
Daniel Krajnak <dan.krajnak@gmail.com>
2019-03-31 00:17:31 -0400

    Revert digit components

--- src/Components/Display/Digit.jsx
+++ src/Components/Display/Digit.jsx
@@ -1,62 +1,43 @@
 // @flow
-import React, { useRef, type ComponentType } from "react";
-import Canvas from "./Canvas.jsx";
-import useHover from "../../Hooks/useHover";
+import React from "react";
+import styled from "styled-components";
 
 type Props = {
-  digit: ?Array<Array<number>>,
-  pixelSize?: ?number
+  digit: ?Array<Array<number>>
 };
-
-export default (React.memo(function Digit({ digit, pixelSize }: Props) {
-  const canvas = useRef(null);
-
-  const size = pixelSize || 8;
-  const artist = (
-    ctx: CanvasRenderingContext2D,
-    coordinates?: { x: number, y: number }
-  ) => {
-    if (digit) {
-      digit.forEach((row, ri) =>
-        row.forEach((pixel, pi) => {
-          if (
-            coordinates &&
-            coordinates.x >= pi * size &&
-            coordinates.x < (pi + 1) * size &&
-            coordinates.y >= ri * size &&
-            coordinates.y < (ri + 1) * size
-          ) {
-            ctx.fillStyle = `red`;
-          } else {
-            ctx.fillStyle = `rgb(${pixel}, ${pixel}, ${pixel})`;
-          }
-
-          ctx.fillRect(pi * size, ri * size, size, size);
-        })
-      );
-    }
-  };
-
-  let handleHover = () => null;
-
-  if (canvas && canvas.current) {
-    const context = canvas.current.getContext("2d");
-    artist(context);
-    handleHover = coordinates =>
-      requestAnimationFrame(() => artist(context, coordinates));
+const Pixel = styled.div.attrs(({ pixel }) => ({
+  background: `rgb(${pixel}, ${pixel}, ${pixel});`
+}))`
+  &:hover {
+    border: solid red 1px;
   }
-  useHover(canvas, handleHover);
+  background: ${props => props.background};
+  color: "white";
+  alignself: "stretch";
+  fontsize: ".8em";
+`;
 
+const Digit = ({ digit }: Props) => {
   if (!digit) {
     return null;
   }
-
+  const columns = digit.length;
+  const rows = digit[0].length;
+  const pixelHeight = 10;
+  const pixelWidth = 10;
   return (
-    <Canvas
-      ref={canvas}
-      artist={artist}
-      width={size * digit.length}
-      height={size * digit.length}
-    />
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `${pixelWidth}px `.repeat(columns),
+        gridTemplateRows: `${pixelHeight}px `.repeat(rows)
+      }}
+    >
+      {digit.map((row, ri) =>
+        row.map((pixel, ci) => <Pixel key={`${ri}-${ci}`} pixel={pixel} />)
+      )}
+    </div>
   );
-}): ComponentType<Props>);
+};
+
+export default Digit;
